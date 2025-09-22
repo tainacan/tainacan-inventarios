2bd970cca0298d4b5801ffe3a1c9f49f220eaae5
mateuswetah <mateus.m.luna@gmail.com>
2025-09-22 14:45:35 -0300

    Remove função de teste de extração de valores.

--- functions.php
+++ functions.php
@@ -13,29 +13,6 @@ const TAINACAN_INVENTARIOS_VERSION = '0.1.0';
 // Evita acesso direto ao arquivo
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
-function teste_extrair_valor($serialized) {
-
-    $array = @unserialize($serialized);
-
-    if (is_array($array)) {
-        // Garantir que todos os valores sejam convertidos para string
-        $strings = array_map(function ($item) {
-            if (is_array($item)) {
-                return '[array]';
-            } elseif (is_object($item)) {
-                return '[objeto]';
-            } else {
-                return (string) $item;
-            }
-        }, $array);
-
-        $legivel = implode(', ', $strings);
-        return $legivel;
-    } else {
-        return 'Formato inválido ou não serializado';
-    }
-}
-
 /* ----------------------------- IMPORTS  ----------------------------- */
 require_once __DIR__ . '/traits/singleton.php';
 
